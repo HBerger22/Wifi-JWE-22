3ab6603b956ca5006beb51553dc35ee86e1cffd9
Helmut <berger.he@gmail.com>
2023-01-17 21:34:37 +0100

    Jquery 1

---
+++ JS/JS_10_jquery/js/main.js
@@ -0,0 +1,81 @@
+"use strict";
+// document.getElementById('headline').style.color = 'red';
+// document.getElementById('headline').style.textDecoration = 'underline';
+// document.getElementById('headline').style.font-size = 'red';
+
+$('#headline').css('background-color','red');
+
+let myVariable = 90
+
+// Setter: mehrere css eigenschaften setzen (attribut + wert = setter)
+$('#headline').css({
+    'color': 'orange',
+    'text-decoration':'underline',
+    'font-size': myVariable +'px'
+
+});
+
+// Getter: Abfragen von css informationen (nur attribut ohne wert = getter), gibt die Farbe von der id headline zurück
+console.log($('#headline').css('color'));
+
+$('.alert').hide();
+/*
+Windows.setTimeout hat 2 Attribute,
+1. die Funktion, die danach ausgeführt werden soll
+2. nach welcher Zeit es ausgeführt werden soll
+*/
+window.setTimeout(
+    function(){
+        $('.alert').slideDown()
+        },2000);
+
+// mehrere befehle hintereinander schreiben
+$('.alert').
+    delay(3000).
+    fadeOut().
+    delay(3000).
+    fadeIn()
+;
+
+// alle buttons in der Klasse alert ansprechen und mit lenght abfragen wieviele es gibt. Wenn es welche gibt, dann bekommen sie die Funktion das wenn sie geklickt werden, wird die Klasse alert ausgeblendet
+ console.log($('.alert button').length);
+
+if($('.alert button').length > 0){
+    $('.alert button').click(function(){
+        $('.alert').fadeOut();
+    })
+};
+
+/*
+Attribute: 
+
+z.B.: bei h1 id='headline'
+*/
+
+let spanTags = $('p span');
+
+console.log(spanTags);
+console.log(spanTags[0].nodeName);
+console.log(spanTags.attr('class','nw')); //hier wird die Klasse kw mit nw ersetzt (console log macht hier natürlich keinen Sinn)
+console.log(spanTags.attr('class'));
+spanTags.addClass('xyz');
+spanTags.removeClass('nw');
+
+// ein Strong Tag in den Span tag einfügen
+spanTags.wrapInner('<strong></strong>');
+
+// ein Strong Tag um den Span tag einfügen
+// spanTags.wrap('<strong></strong>');
+
+spanTags.each(
+    function(index, element){
+        console.log(element)
+        console.log($(element).html()); // alles was innerhalb des Span tags steht auch tags selber
+        console.log($(element).text()); // alles was innerhalb des Span tags steht ohne tags selber
+        $(element).append('<sup>'+(index +1)+'</sup> '); //füge den index hochgestellt dazu
+        $(element).find('sup').css('color','red');
+    }
+)
+
+
+
---
+++ JS/JS_11_image_galery/js/main.js
@@ -0,0 +1,25 @@
+let prototyp ='<a href="images/1.jpg"><img src="images/thumbs/1.jpg" alt="Bild 1"></a>"';
+
+let proto =` 
+    <a href="images/#.jpg">
+        <img src="images/thumbs/#.jpg" alt="Bild #">
+    </a>
+`; //schräger einfacher Anführungsstrich geht über mehrere Zeilen (shift + neben ?)
+
+console.log(proto);
+
+$(proto).find('img').attr('src');
+console.log($(proto).find('img').attr('src'));
+
+let images = [
+    1,
+    2,
+    3,
+    4
+]
+
+$(images).each(function(index, element){
+    const html=$(proto);
+    html.find('img').attr('src'), replace('#',element);
+    $('body').append(html);
+});
